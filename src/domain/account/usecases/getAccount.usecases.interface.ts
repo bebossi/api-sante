d31e87e5eca3bfbbbb83eb16b4511647e1ecca6f
bebossi <bernardobossi77@gmail.com>
2024-03-15 12:05:03 +0000

    refactor: change folder name to user

--- src/domain/account/usecases/getAccount.usecases.interface.ts
+++
@@ -1,5 +0,0 @@
-import { AccountModel } from '../entity/account'
-
-export interface IGetAccountUsecases {
-  execute(id: number): Promise<AccountModel>
-}
--- src/domain/user/usecases/createAccount.usecases.interface.ts
+++ src/domain/user/usecases/createAccount.usecases.interface.ts
@@ -2,6 +2,7 @@ export interface CreateAccountUsecaseInput {
   name: string
   email: string
   password: string
+  role: string
 }
 
 export interface ICreateAccountUsecases {
