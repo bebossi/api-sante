388d1a4a6325cb33b38ae206b75462500f64f6b8
bebossi <bernardobossi77@gmail.com>
2024-03-21 18:24:15 +0000

    refactor: add interfaces to usecases file

--- src/app/user/usecases/create-user.usecases.ts
+++ src/app/user/usecases/create-user.usecases.ts
@@ -1,27 +1,35 @@
 import { Encrypt } from '../../../domain/@shared/utils/encrypt'
 import { User } from '../../../domain/user/entity/user'
 import { EmailAlreadyInUseError } from '../../../domain/user/errors/email-already-in-use-error'
-import { IAccountRepository } from '../../../domain/user/repository/user.repository.interface'
-import {
-  ICreateUserUsecases,
-  CreateUserUsecaseInput,
-} from '../../../domain/user/usecases/createAccount.usecases.interface'
+import { IUserRepository } from '../../../domain/user/repository/user.repository.interface'
+
+export interface CreateUserUsecaseInput {
+  name: string
+  email: string
+  password: string
+  role: string
+}
+
+export interface ICreateUserUsecases {
+  execute(input: CreateUserUsecaseInput): Promise<void>
+}
 
 export class CreateUserUsecase implements ICreateUserUsecases {
-  constructor(private readonly accountRepository: IAccountRepository) {}
+  constructor(private readonly userRepository: IUserRepository) {}
   async execute(input: CreateUserUsecaseInput): Promise<void> {
     const { name, email, password, role } = input
 
-    const findUser = await this.accountRepository.findByEmail(email)
+    const findUser = await this.userRepository.findByEmail(email)
     if (findUser) throw new EmailAlreadyInUseError()
 
     const encryptedPassword = await Encrypt.encryptPassword(password)
+
     const user = User.create({
       name,
       email,
       password: encryptedPassword,
       role,
     })
-    await this.accountRepository.create(user)
+    await this.userRepository.create(user)
   }
 }
