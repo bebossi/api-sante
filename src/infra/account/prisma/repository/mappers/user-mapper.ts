ff00c23534afde4a4c83d3dec54beff5524a6883
bebossi <bernardobossi77@gmail.com>
2024-03-21 18:27:59 +0000

    feat: add UserMapper to transfer data between entity and prisma

---
+++ src/infra/account/prisma/repository/mappers/user-mapper.ts
@@ -0,0 +1,34 @@
+import { User as PrismaUser } from '@prisma/client'
+import { User } from '../../../../../domain/user/entity/user'
+
+class UserMapper {
+  public static toPrisma(user: User) {
+    const map = {
+      id: user.id.value,
+      name: user.name,
+      password: user.password,
+      email: user.email,
+      role: user.role,
+      created_at: user.createdAt,
+      updated_at: user.updatedAt,
+    }
+
+    return map
+  }
+
+  public static toEntity(input: PrismaUser): User {
+    const user = User.create({
+      id: input.id,
+      email: input.email!,
+      password: input.password!,
+      name: input.name!,
+      role: input.role,
+      createdAt: input.created_at,
+      updatedAt: input.updated_at,
+    })
+
+    return user
+  }
+}
+
+export { UserMapper }
