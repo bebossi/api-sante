ee755972523b65e4a37516bcdaa42ec9e85892ae
bebossi <bernardobossi77@gmail.com>
2024-03-22 17:24:46 +0000

    feat: add UserNotFounError

---
+++ src/domain/user/errors/user-not-found-error.ts
@@ -0,0 +1,11 @@
+class UserNotFoundError extends Error {
+  constructor(message = 'User not found') {
+    super(message)
+    Object.setPrototypeOf(this, new.target.prototype)
+    this.name = this.constructor.name
+    Error.captureStackTrace(this, this.constructor)
+    this.message = message
+  }
+}
+
+export { UserNotFoundError }
