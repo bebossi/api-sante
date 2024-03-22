2cef449d798188f8f0fb0c2e24a01b68d1bf16ee
bebossi <bernardobossi77@gmail.com>
2024-03-22 17:24:26 +0000

    feat: add LoginUsecase

---
+++ src/app/user/usecases/login/login.usecase.ts
@@ -0,0 +1,51 @@
+import { Encrypt } from '@domain/@shared/utils/encrypt'
+import { injectable, inject } from 'tsyringe'
+import jwt from 'jsonwebtoken'
+import { IUserRepository } from '@domain/user/repository/user.repository.interface'
+import { WrongCredentialsError } from '@domain/user/errors/wrong-credentials-error'
+import { UserNotFoundError } from '@domain/user/errors/user-not-found-error'
+
+interface LoginUseCaseInput {
+  email: string
+  password: string
+}
+
+interface LoginUseCaseOutput {
+  token: string
+}
+
+@injectable()
+class LoginUseCase {
+  constructor(
+    @inject('IUserRepository')
+    private userRepository: IUserRepository
+  ) {}
+
+  public async execute({
+    email,
+    password,
+  }: LoginUseCaseInput): Promise<LoginUseCaseOutput> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) throw new UserNotFoundError()
+
+    const isValidPassword = await Encrypt.compare(password, user?.password as string)
+
+    if (!isValidPassword) throw new WrongCredentialsError()
+
+    await this.userRepository.login({
+      email,
+      password,
+    })
+
+    const token = jwt.sign({}, process.env.TOKEN_SIGN_SECRET as string, {
+      expiresIn: '24h',
+      subject: user.id.value,
+    })
+    console.log('usecase', token)
+
+    return { token: token }
+  }
+}
+
+export { LoginUseCase }
