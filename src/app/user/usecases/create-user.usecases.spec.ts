19e49fe37d2ffc7661f697763935085dbd7b7efd
bebossi <bernardobossi77@gmail.com>
2024-03-22 11:56:26 +0000

    test: ensure EmailAlreadyInUseError is called when create a new user with an existins email

--- src/app/user/usecases/create-user.usecases.spec.ts
+++ src/app/user/usecases/create-user.usecases.spec.ts
@@ -1,6 +1,8 @@
 import { FakeUsersRepository } from '@domain/user/repository/fake/fake-user-repository'
 import { CreateUserUsecase } from './create-user.usecases'
 import { Encrypt } from '@domain/@shared/utils/encrypt'
+import { User } from '@domain/user/entity/user'
+import { EmailAlreadyInUseError } from '@domain/user/errors/email-already-in-use-error'
 
 describe('RegisterUseCase unit tests', () => {
   let usersRepository: FakeUsersRepository
@@ -42,4 +44,26 @@ describe('RegisterUseCase unit tests', () => {
     expect(createUserSpy).toHaveBeenCalled()
     expect(encrypSpy).toHaveBeenCalled()
   }, 100000)
+
+  it('should throw error if email is already in use', async () => {
+    const firstRegister = {
+      name: 'Bernardo',
+      email: 'bernardo@gmail.com',
+      password: 'test123',
+      role: 'admin',
+    }
+
+    const user = User.create(firstRegister)
+
+    await usersRepository.create(user)
+
+    const secondRegister = {
+      name: 'Bernaewrgrdo',
+      email: 'bernardo@gmail.com',
+      password: 'testegwegw123',
+      role: 'user',
+    }
+
+    await expect(usecase.execute(secondRegister)).rejects.toThrow(EmailAlreadyInUseError)
+  })
 })
