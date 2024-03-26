e384e4a1646ca522918ad8fbbdd4621fd85886df
bebossi <bernardobossi77@gmail.com>
2024-03-26 11:49:06 +0000

    feat: create GetUserDataUsecase class to retrive user data

---
+++ src/app/user/usecases/get-user-data/get-user-data.ts
@@ -0,0 +1,33 @@
+import { UserNotFoundError } from '@domain/user/errors/user-not-found-error'
+import { IUserRepository } from '@domain/user/repository/user.repository.interface'
+import { inject, injectable } from 'tsyringe'
+
+interface GetUserDataInput {
+  userId: string
+}
+
+interface GetUserDataOutput {
+  id: string
+  email: string
+  name: string
+  role: string
+  createdAt: string
+  updatedAt: string
+}
+
+@injectable()
+class GetUserDataUsecase {
+  constructor(
+    @inject('UsersRepositoryInterface')
+    private usersRepository: IUserRepository
+  ) {}
+
+  public async execute({ userId }: GetUserDataInput): Promise<GetUserDataOutput> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) throw new UserNotFoundError()
+    return { ...user.toJSON() }
+  }
+}
+
+export { GetUserDataUsecase }
