af1e9c6f803971fa26de552418fa4485d725a48c
bebossi <bernardobossi77@gmail.com>
2024-04-06 14:01:18 +0100

    feat: add notification and deepFreeze method do value-object class

--- src/domain/@shared/value-object.ts
+++ src/domain/@shared/value-object.ts
@@ -1,10 +1,17 @@
 import isEqual from 'lodash/isEqual'
+import Notification from './notification/notification'
 
 export abstract class ValueObject<Value = any> {
   protected readonly _value: Value
+  protected _notification: Notification
 
   constructor(value: Value) {
-    this._value = value
+    this._value = deepFreeze(value)
+    this._notification = new Notification()
+  }
+
+  get notification() {
+    return this._notification
   }
 
   get value(): Value {
@@ -27,12 +34,27 @@ export abstract class ValueObject<Value = any> {
   toString = () => {
     if (typeof this.value !== 'object' || this.value === null) {
       try {
-        return String(this.value)
+        return this.value
       } catch (e) {
         return this.value + ''
       }
     }
     const valueStr = this.value.toString()
     return valueStr === '[object Object]' ? JSON.stringify(this.value) : valueStr
+  }
+}
+
+export function deepFreeze<T>(obj: T) {
+  try {
+    const propNames = Object.getOwnPropertyNames(obj)
+    for (const name of propNames) {
+      const value = obj[name as keyof T]
+      if (value && typeof value === 'object') {
+        deepFreeze(value)
+      }
+    }
+    return Object.freeze(obj)
+  } catch (e) {
+    return obj
   }
 }
