d4d3bf60af5654b237589719528e108241179249
bebossi <bernardobossi77@gmail.com>
2024-04-06 14:23:44 +0100

    refactor: tranform Product type into a class

---
+++ src/domain/product/entity/product/product.ts
@@ -0,0 +1,108 @@
+import { AggregateRoot } from '@domain/@shared/aggegate-root'
+import ProductValidatorFactory from '../../factories/validators/product-validator.factory'
+import { ProductId } from '../../value-objects/product-id'
+import { Category } from '../category'
+import { ToppingModel } from '../topping'
+import NotificationError from '@domain/@shared/notification/notification-error'
+
+export type ProductProps = {
+  id: ProductId
+  name: string
+  description: string
+  price: number
+  image?: string
+  categoryId: string
+  category: Category
+  toppings: ToppingModel[]
+  createdAt: Date
+  updatedAt: Date
+}
+export type ProductConstructorProps = {
+  id?: ProductId | string
+  name: string
+  description: string
+  price: number
+  image?: string
+  categoryId: string
+  toppings: ToppingModel[]
+  updatedAt: any
+  createdAt: string | number | Date
+}
+
+class Product extends AggregateRoot {
+  _props: ProductProps = {} as ProductProps
+  constructor(props: ProductConstructorProps) {
+    super()
+    this._props.id =
+      typeof props.id === 'string' ? new ProductId(props.id) : props.id ?? new ProductId()
+    this._id = this._props.id
+    this._props.name = props.name
+    this._props.description = props.description
+    this._props.price = props.price
+    this._props.image = props.image
+    this._props.categoryId = props.categoryId
+    this._props.toppings = props.toppings
+    this._props.createdAt = props.createdAt ? new Date(props.createdAt) : new Date()
+    this._props.updatedAt = props.updatedAt
+      ? new Date(props.updatedAt)
+      : this._props.createdAt
+  }
+
+  public static create(input: ProductConstructorProps): Product {
+    const product = new Product(input)
+    product.validate()
+    return product
+  }
+
+  public validate() {
+    ProductValidatorFactory.create().validate(this)
+    if (this._notification.hasErrors()) {
+      throw new NotificationError(this._notification.getErrors())
+    }
+  }
+  get id() {
+    return this._props.id
+  }
+  get name() {
+    return this._props.name
+  }
+  get description() {
+    return this._props.description
+  }
+
+  get price() {
+    return this._props.price
+  }
+  get image() {
+    return this._props.image
+  }
+
+  get toppings() {
+    return this._props.toppings
+  }
+  get categoryId() {
+    return this._props.categoryId
+  }
+  get createdAt() {
+    return this._props.createdAt
+  }
+  get updatedAt() {
+    return this._props.updatedAt
+  }
+
+  toJSON() {
+    return {
+      id: this._props.id,
+      name: this._props.name,
+      description: this._props.description,
+      price: this._props.price,
+      image: this._props.image,
+      toppings: this._props.toppings,
+      categoryId: this._props.categoryId,
+      createdAt: this._props.createdAt.toISOString(),
+      updatedAt: this._props.updatedAt.toISOString(),
+    }
+  }
+}
+
+export { Product }
