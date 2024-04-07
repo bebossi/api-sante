47d9cd6a32a77b9c16a757b61122d502ba791212
bebossi <bernardobossi77@gmail.com>
2024-04-07 12:26:32 +0100

    feat: add CategoryNotFounError

---
+++ src/domain/product/errors/category-not-found-error.ts
@@ -0,0 +1,11 @@
+class CategoryNotFoundError extends Error {
+  constructor(message = 'Category not found') {
+    super(message)
+    Object.setPrototypeOf(this, new.target.prototype)
+    this.name = this.constructor.name
+    Error.captureStackTrace(this, this.constructor)
+    this.message = message
+  }
+}
+
+export { CategoryNotFoundError }
