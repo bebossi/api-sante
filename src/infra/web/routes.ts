2c74a1e07d934e638db1613dbfeb657bccf78373
bebossi <bernardobossi77@gmail.com>
2024-04-07 12:34:33 +0100

    feat: add productRoute to appRouter

--- src/infra/web/routes.ts
+++ src/infra/web/routes.ts
@@ -1,8 +1,10 @@
+import { productRouter } from './product/routes/product.router'
 import { router } from './trpc'
 import { userRouter } from './user/routes/user.router'
 
 export const appRouter = router({
   user: userRouter,
+  product: productRouter,
 })
 
 export type AppRouter = typeof appRouter
