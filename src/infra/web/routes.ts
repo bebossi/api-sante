f9834caffeba929e8e52c4a55439db84a1d7113a
bebossi <bernardobossi77@gmail.com>
2024-04-07 12:41:28 +0100

    feat: add categoryRouter to appRouter

--- src/infra/web/routes.ts
+++ src/infra/web/routes.ts
@@ -1,3 +1,4 @@
+import { categoryRouter } from './product/routes/category.router'
 import { productRouter } from './product/routes/product.router'
 import { router } from './trpc'
 import { userRouter } from './user/routes/user.router'
@@ -5,6 +6,7 @@ import { userRouter } from './user/routes/user.router'
 export const appRouter = router({
   user: userRouter,
   product: productRouter,
+  category: categoryRouter,
 })
 
 export type AppRouter = typeof appRouter
