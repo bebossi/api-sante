dde3a20c328d21404332f45f12f60a871a8a90ef
bebossi <bernardobossi77@gmail.com>
2024-04-09 15:53:41 +0100

    feat: add ProductNotFound

---
+++ src/domain/product/errors/product-not-found.ts
@@ -0,0 +1,11 @@
+class ProductNotFound extends Error {
+  constructor(message = 'Product not found') {
+    super(message)
+    Object.setPrototypeOf(this, new.target.prototype)
+    this.name = this.constructor.name
+    Error.captureStackTrace(this, this.constructor)
+    this.message = message
+  }
+}
+
+export { ProductNotFound }
