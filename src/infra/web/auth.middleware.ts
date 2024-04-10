6276afb5134e50c1b832e451d000f9d1ef742b57
bebossi <bernardobossi77@gmail.com>
2024-04-10 08:57:48 +0100

    feat: add isAdminMiddleware

--- src/infra/web/auth.middleware.ts
+++ src/infra/web/auth.middleware.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 import { middleware, publicProcedure } from './trpc'
 
 const isLoggedMiddleware = middleware(async ({ ctx, next }) => {
-  // console.log(ctx.token)
   if (!ctx.token) {
     throw new TRPCError({ code: 'UNAUTHORIZED' })
   }
@@ -17,6 +16,32 @@ const isLoggedMiddleware = middleware(async ({ ctx, next }) => {
   }
 })
 
-const authorizedProcedure = publicProcedure.use(isLoggedMiddleware)
+const isAdminMiddleware = middleware(async ({ ctx, next }) => {
+  if (!ctx.token) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+  try {
+    const decoded = jwt.verify(ctx.token, process.env.TOKEN_SIGN_SECRET as string)
 
-export { authorizedProcedure }
+    if (typeof decoded === 'object' && decoded && 'id' in decoded && 'role' in decoded) {
+      const { id, role } = decoded
+
+      if (role !== 'admin') {
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+      }
+
+      return next({
+        ctx: { user_id: id as string },
+      })
+    } else {
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
+    }
+  } catch (error) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+})
+
+const authorizedProcedure = publicProcedure.use(isLoggedMiddleware)
+const adminProcedure = publicProcedure.use(isAdminMiddleware)
+
+export { authorizedProcedure, adminProcedure }
